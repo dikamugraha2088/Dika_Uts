e10b60c177cf89b16ce8b7687255be834163ab3b
WP2-dika20 <dikanugraha2088@gmail.com>
2021-10-31 13:40:47 +0700

    menambahkan toko-sepatu

--- application/views/prauts/template/foot.php
+++ application/views/prauts/template/foot.php
@@ -1,7 +1,7 @@
 
     </div>
     <div class="card-footer text-muted" style="position: fixed; bottom: 0; width: 100%; text-align: center;">
-      Copyright <a href="https://github.com/lemonsprite">Ela Nurlela</a> &copy; 2020
+      Copyright <a href="https://github.com/lemonsprite">Dika nugraha</a> &copy; 2020
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
